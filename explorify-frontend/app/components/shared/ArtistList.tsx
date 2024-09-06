7c816b9869638b85a8825e13f49a346146ca91ce
Daniel Mesu <danielrojo1927@gmail.com>
2024-09-06 05:34:55 +0100

    formatting top items response

--- explorify-frontend/app/components/shared/ArtistList.tsx
+++ explorify-frontend/app/components/shared/ArtistList.tsx
@@ -1,21 +1,23 @@
+import { ProfileArtistsList } from "@/app/interface/profile";
+
 interface ArtistListProps {
   title: string;
-  artists: { name: string; count: number }[];
+  artists: ProfileArtistsList[];
 }
 
 export const ArtistList: React.FC<ArtistListProps> = ({ title, artists }) => {
   return (
-    <div className='container mx-auto p-3'>
-      <h1 className='text-3xl text-black font-bold mb-4'>{title}</h1>
+    <div className='container mx-auto p-2'>
+      <h1 className='text-4xl text-black font-bold mb-4'>{title}</h1>
       <ul>
         {artists.map((artist, index) => (
-          <li key={index} className='mb-4'>
+          <li key={index} className='m-1'>
             <div className='flex justify-between'>
               <span className='text-lg text-black font-semibold'>
                 {artist.name}
               </span>
               <span className='text-lg text-black font-medium'>
-                {artist.count}
+                {artist.followers}
               </span>
             </div>
             <hr className='border-t border-black mt-2' />
--- explorify-frontend/app/utils/auth/auth.utils.ts
+++ explorify-frontend/app/utils/auth/auth.utils.ts
@@ -1,13 +1,13 @@
 import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 
-export const getAccessToken = () => {
+export const getAccessToken = (): string | null => {
   const params = new URLSearchParams(window.location.search);
   const accessToken = params.get("access_token");
 
   return accessToken;
 };
 
-export const getRefreshToken = () => {
+export const getRefreshToken = (): string | null => {
   const params = new URLSearchParams(window.location.search);
   const refreshToken = params.get("refresh_token");
 
@@ -27,7 +27,10 @@ export const axiosRequest = <T = any>(
   return axios.request<T>(config);
 };
 
-export const preserveQueryParameters = (basePath: string, query: any) => {
+export const preserveQueryParameters = (
+  basePath: string,
+  query: any
+): string => {
   const queryString = new URLSearchParams(query).toString();
   return `${basePath}?${queryString}`;
 };
